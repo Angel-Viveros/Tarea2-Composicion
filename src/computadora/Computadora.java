bdac73cb5ff33c59ba3659e6f32704f77a0a35f8
angel <angelviveros318@aragon.unam.mx>
2021-06-22 16:56:04 -0500

    Computadora1

--- src/computadora/Computadora.java
+++ src/computadora/Computadora.java
@@ -20,7 +20,7 @@ public class Computadora {
  private int marca;
  private int modelo;
 
-    /**
+    /*
      * @param args the command line arguments
      */
     public static void main(String[] args) {
